b50264e74a4c110e04a57abf17b9915bf6e45333
szayo <filippalach@gmail.com>
2019-01-31 12:56:01 -0800

    WIP, getopts added

--- cppmake.sh
+++ cppmake.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-path=`pwd`
-DIR_EXS=99
-NO_ARG=100
+function main(){
+  local OPTIND=1 opt 
+  while getopts ":p:h:" opt; do
+    case $opt in 
+      p) input="$OPTARG"; 
+	 mkdir -p $input.pro/{build,headers,sources}
+	 touch $input.pro/CMakeLists.txt
+	 touch $input.pro/sources/main.cpp 
+	 tree $input.pro ;;
+	 #set_var ;;
+      h) echo "wybrales h";;
+      \?) read_help ;;
+    esac
+  done
+  shift $((OPTIND-1))
+}
 
-cd $path
+function set_var(){
+  var="export $input=`pwd`/$input.pro"
+  cd $HOME
+  echo $var >> .bashrc 
+}
 
-if [ $# -eq 1 ]
-then
-	if [ ! -d "$1.cppproject" ]
-	then
-		mkdir $1.projectcpp
-		cd $1.projectcpp
-		mkdir build sources headers
-		: > CMakeLists.txt
-		tree
-	else
-		echo "Directory name already exists."; echo
-		exit $DIR_EXS
-	fi 
-else
-	echo "No arguments - type 1"; echo
-	exit $NO_ARG
-fi	
-exit 0
+function read_help(){
+  echo "Error: Bad use of parameter"
+  echo "Use -p for project making and -h for adding header"
+}
+
+main $@
