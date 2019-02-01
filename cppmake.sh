c32c3c1c791814abaaaac60d9aae3ea3d65840de
szayo <filippalach@gmail.com>
2019-02-01 11:31:27 -0800

    Added -h, tested

--- cppmake.sh
+++ cppmake.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
 function main(){
+
+  class_exists=45
+  
   local OPTIND=1 opt 
-  while getopts ":p:h:" opt; do
+
+  while getopts "p:h:" opt; do
     case $opt in 
-      p) input="$OPTARG"; 
+      p) input="$OPTARG"
 	 mkdir -p $input.pro/{build,headers,sources}
 	 touch $input.pro/CMakeLists.txt
 	 touch $input.pro/sources/main.cpp 
-	 tree $input.pro ;;
-	 #set_var ;;
-      h) echo "wybrales h";;
+	 tree $input.pro
+	 ;;
+      h) input="$OPTARG" 
+	 if [ ! -e headers/$input.h ] && [ ! -e sources/$input.cpp ] && [ -d headers ] && [ -d sources ]
+	 then
+	   cd headers
+	   : > $input.h
+	   cd ..; cd sources
+	   : > $input.cpp
+	 else
+	   echo "This class already exists in working project or bad directory."
+	   exit $class_exists 
+	 fi
+	 ;; 
       \?) read_help ;;
     esac
   done
   shift $((OPTIND-1))
-}
-
-function set_var(){
-  var="export $input=`pwd`/$input.pro"
-  cd $HOME
-  echo $var >> .bashrc 
 }
 
 function read_help(){
