cfaefb82cf8110bc76c55ae2e9c9e462df637e6b
szayo <filippalach@gmail.com>
2019-02-09 12:13:22 -0800

    full template, small fixes

--- cppmake.sh
+++ cppmake.sh
@@ -3,23 +3,34 @@
 function main(){
 
   class_exists=45
+  dir_exists=46
   
   local OPTIND=1 opt 
 
-  while getopts "p:h:" opt; do
+  while getopts ":p:h:" opt; do
     case $opt in 
       p) input="$OPTARG"
-	 mkdir -p $input.pro/{build,headers,sources}
-	 touch $input.pro/CMakeLists.txt
-	 touch $input.pro/sources/main.cpp 
-	 cd $input.pro
-	 	echo "cmake_minimum_required(VERSION 2.8.9)" >> CMakeLists.txt
-	 	echo "project($input)" >> CMakeLists.txt; echo "" >> CMakeLists.txt
-	 	echo "include_directories(headers)" >> CMakeLists.txt; echo "" >> CMakeLists.txt 
-	 	echo "file (GLOB SOURCES \"sources/*.cpp\")" >> CMakeLists.txt; echo "" >> CMakeLists.txt
-	 	echo "add_executable($input \${SOURCES})" >> CMakeLists.txt
-	 cd ..
-	 tree $input.pro
+	 if [ ! -d "$input.pro" ] 
+	 then
+	   mkdir -p $input.pro/{build,headers,sources}
+	   touch $input.pro/CMakeLists.txt
+	   touch $input.pro/sources/main.cpp 
+	   cd $input.pro
+		  echo "cmake_minimum_required(VERSION 2.8.9)" >> CMakeLists.txt
+		  echo "project($input)" >> CMakeLists.txt; echo "" >> CMakeLists.txt
+		  echo "include_directories(headers)" >> CMakeLists.txt; echo "" >> CMakeLists.txt 
+		  echo "file (GLOB SOURCES \"sources/*.cpp\")" >> CMakeLists.txt; echo "" >> CMakeLists.txt
+		  echo "add_executable($input \${SOURCES})" >> CMakeLists.txt
+	   cd sources 
+		  echo "#include <iostream>" >> main.cpp; echo "" >> main.cpp
+		  echo "int main(){" >> main.cpp; echo "" >> main.cpp; echo "" >> main.cpp; echo "}" >> main.cpp
+	   cd ..; 
+	   tree 
+	   exec bash
+	 else
+	   echo "This directory name already exists. Choose another name."
+	   exit $dir_exists
+	 fi
 	 ;;
       h) input="$OPTARG" 
 	 if [ ! -e headers/$input.h ] && [ ! -e sources/$input.cpp ] && [ -d headers ] && [ -d sources ]
@@ -28,6 +39,7 @@ function main(){
 	   : > $input.h
 	   cd ..; cd sources
 	   : > $input.cpp
+	   echo "#include \"$input.h\"" >> $input.cpp
 	 else
 	   echo "This class already exists in working project or bad directory."
 	   exit $class_exists 
