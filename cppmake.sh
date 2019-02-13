88c3595b96d0264334c59752d955dd8983bd0ba8
szayo <filippalach@gmail.com>
2019-02-13 15:18:37 -0800

    printf instead of echo - clear code

--- cppmake.sh
+++ cppmake.sh
@@ -16,19 +16,19 @@ function main(){
 	   touch $input.pro/CMakeLists.txt
 	   touch $input.pro/sources/main.cpp 
 	   cd $input.pro
-		  echo "cmake_minimum_required(VERSION 2.8.9)" >> CMakeLists.txt
-		  echo "project($input)" >> CMakeLists.txt; echo "" >> CMakeLists.txt
-		  echo "include_directories(headers)" >> CMakeLists.txt; echo "" >> CMakeLists.txt 
-		  echo "file (GLOB SOURCES \"sources/*.cpp\")" >> CMakeLists.txt; echo "" >> CMakeLists.txt
-		  echo "add_executable($input \${SOURCES})" >> CMakeLists.txt
+		  printf "cmake_minimum_required(VERSION 2.8.9)" >> CMakeLists.txt
+		  printf "\nproject($input)" >> CMakeLists.txt 
+		  printf "\ninclude_directories(headers)" >> CMakeLists.txt 
+		  printf "\nfile (GLOB SOURCES \"sources/*.cpp\")" >> CMakeLists.txt
+		  printf "\nadd_executable($input \${SOURCES})" >> CMakeLists.txt
 	   cd sources 
-		  echo "#include <iostream>" >> main.cpp; echo "" >> main.cpp
-		  echo "int main(){" >> main.cpp; echo "" >> main.cpp; echo "" >> main.cpp; echo "}" >> main.cpp
+		  printf "#include <iostream>" >> main.cpp
+		  printf "\n\nint main(){" >> main.cpp  
+		  printf "\n\n\n}" >> main.cpp
 	   cd ..; 
 	   tree 
-	   #exec bash
 	 else
-	   echo "This directory name already exists. Choose another name."
+	   printf "This directory name already exists. Choose another name."
 	   exit $dir_exists
 	 fi
 	 ;;
@@ -41,9 +41,9 @@ function main(){
 	   : > $input.h
 	   cd ..; cd sources
 	   : > $input.cpp
-	   echo "#include \"$input.h\"" >> $input.cpp
+	   printf "#include \"$input.h\"" >> $input.cpp
 	 else
-	   echo "This class already exists in working project or bad directory."
+	   printf "This class already exists in working project or bad directory."
 	   exit $class_exists 
 	 fi
 	 ;; 
@@ -54,9 +54,9 @@ function main(){
 }
 
 function read_help(){
-  echo "Error: Bad use of parameter"
-  echo "Use -p for project making and -h for adding header"
-  echo "Add further -e after -p parameter to end in project dir; ex: cppmake.sh -p Dir -e"
+  printf "Error: Bad use of parameter"
+  printf "Use -p for project making and -h for adding header"
+  printf "Add further -e after -p parameter to end in project dir; ex: cppmake.sh -p Dir -e"
 }
 
 main $@
